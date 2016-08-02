705d3b78df0417a1d8c4a6ccf85b2980a1d4b901
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-08-02 14:02:52 -0700

    mm/kasan: get rid of ->state in struct kasan_alloc_meta
    The state of object currently tracked in two places - shadow memory, and
    the ->state field in struct kasan_alloc_meta.  We can get rid of the
    latter.  The will save us a little bit of memory.  Also, this allow us
    to move free stack into struct kasan_alloc_meta, without increasing
    memory consumption.  So now we should always know when the last time the
    object was freed.  This may be useful for long delayed use-after-free
    bugs.
    
    As a side effect this fixes following UBSAN warning:
    	UBSAN: Undefined behaviour in mm/kasan/quarantine.c:102:13
    	member access within misaligned address ffff88000d1efebc for type 'struct qlist_node'
    	which requires 8 byte alignment
    
    Link: http://lkml.kernel.org/r/1470062715-14077-5-git-send-email-aryabinin@virtuozzo.com
    Reported-by: kernel test robot <xiaolong.ye@intel.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit b3cbd9bf77cd1888114dbee1653e79aa23fd4068)
    Signed-off-by: Jeremy Huang <jeremyhcw@gmail.com>
    
    Conflicts:
    	mm/slab.c

--- mm/slab.c
+++ mm/slab.c
@@ -2528,8 +2528,45 @@ static void kmem_flagcheck(struct kmem_cache *cachep, gfp_t flags)
 	}
 }
 
+<<<<<<< HEAD
 static void *slab_get_obj(struct kmem_cache *cachep, struct page *page,
 				int nodeid)
+=======
+static void cache_init_objs(struct kmem_cache *cachep,
+			    struct page *page)
+{
+	int i;
+	void *objp;
+	bool shuffled;
+
+	cache_init_objs_debug(cachep, page);
+
+	/* Try to randomize the freelist if enabled */
+	shuffled = shuffle_freelist(cachep, page);
+
+	if (!shuffled && OBJFREELIST_SLAB(cachep)) {
+		page->freelist = index_to_obj(cachep, page, cachep->num - 1) +
+						obj_offset(cachep);
+	}
+
+	for (i = 0; i < cachep->num; i++) {
+		objp = index_to_obj(cachep, page, i);
+		kasan_init_slab_obj(cachep, objp);
+
+		/* constructor could break poison info */
+		if (DEBUG == 0 && cachep->ctor) {
+			kasan_unpoison_object_data(cachep, objp);
+			cachep->ctor(objp);
+			kasan_poison_object_data(cachep, objp);
+		}
+
+		if (!shuffled)
+			set_free_obj(page, i, i);
+	}
+}
+
+static void *slab_get_obj(struct kmem_cache *cachep, struct page *page)
+>>>>>>> b3cbd9b... mm/kasan: get rid of ->state in struct kasan_alloc_meta
 {
 	void *objp;
 
