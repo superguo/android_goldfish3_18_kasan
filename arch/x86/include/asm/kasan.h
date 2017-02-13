45305b4a50ef20c18799e98a0365953fc9f80923
Jeremy Huang <jeremyhcw@gmail.com>
2017-02-13 07:53:23 -0500

    Manully, x86/kasan: generate KASAN_SHADOW_OFFSET in Makefile from : [v4,1/7] x86/kasan: generate KASAN_SHADOW_OFFSET in Makefile [v4,2/7] mm: kasan: introduce generic kasan_populate_zero_shadow() [v4,7/7] x86/kasan: switch to generic kasan_populate_zero_shadow()

--- arch/x86/include/asm/kasan.h
+++ arch/x86/include/asm/kasan.h
@@ -4,28 +4,17 @@
 #include <linux/const.h>
 #define KASAN_SHADOW_OFFSET _AC(CONFIG_KASAN_SHADOW_OFFSET, UL)
 
-/*
- * Compiler uses shadow offset assuming that addresses start
- * from 0. Kernel addresses don't start from 0, so shadow
- * for kernel really starts from compiler's shadow offset +
- * 'kernel address space start' >> KASAN_SHADOW_SCALE_SHIFT
- */
-#define KASAN_SHADOW_START      (KASAN_SHADOW_OFFSET + \
-					(0xffff800000000000ULL >> 3))
+#define KASAN_SHADOW_START      (0xffffec0000000000ULL)
 /* 47 bits for kernel address -> (47 - 3) bits for shadow */
 #define KASAN_SHADOW_END        (KASAN_SHADOW_START + (1ULL << (47 - 3)))
 
 #ifndef __ASSEMBLY__
 
-extern pte_t kasan_zero_pte[];
-extern pte_t kasan_zero_pmd[];
-extern pte_t kasan_zero_pud[];
-
 #ifdef CONFIG_KASAN
-void __init kasan_map_early_shadow(pgd_t *pgd);
+void __init kasan_early_init(void);
 void __init kasan_init(void);
 #else
-static inline void kasan_map_early_shadow(pgd_t *pgd) { }
+static inline void kasan_early_init(void) { }
 static inline void kasan_init(void) { }
 #endif
 
